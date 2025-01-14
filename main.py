1b2dfe6b0978071578387f0c0c2f35cce57b75a6
Lenovo <ulyana.a.linnik@gmail.com>
2025-01-14 18:40:17 +0300

    third number

--- main.py
+++ main.py
@@ -1,55 +1,40 @@
+import os
 import sys
 
-from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QPainter, QColor, QPen, QBrush
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
-from random import randint
-
-
-class MyWidget(QWidget):
-
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.setGeometry(300, 300, 300, 300)
-        self.setWindowTitle("")
-
-        self.trick_button = QPushButton('draw', self)
-        self.trick_button.resize(40,40)
-        self.trick_button.move(125, 100)
-        self.trick_button.clicked.connect(self.paint)
-        self.do_paint = False
-
-    def paintEvent(self, event):
-        if self.do_paint:
-            qp = QPainter()
-            qp.begin(self)
-            self.draw_flag(qp)
-            qp.end()
-
-    def paint(self):
-        self.do_paint = True
-        self.update()
-
-    def draw_flag(self, qp):
-        painter = QPainter(self)
-        r = randint(0, 255)
-        g = randint(0,255)
-        b = randint(0, 255)
-        pen = QPen(QColor(r, g, b), 3)  # Yellow color, 3px thickness
-        painter.setPen(pen)
-
-        # Draw the circle
-        center_x = randint(0, 300)
-        center_y = randint(0,300)
-        radius = randint(0, 50)
-        painter.drawEllipse(center_x - radius, center_y - radius, radius * 2, radius * 2)
-
+import pygame
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MyWidget()
-    ex.show()
-    sys.exit(app.exec())
+    pygame.init()
+    pygame.display.set_caption("крест")
+
+    size = width, height = 300, 200
+
+    screen = pygame.display.set_mode(size)
+    screen.fill((255, 255, 255))
+
+    w = 32
+    h = 17
+    hw = 15
+    all_sprites = pygame.sprite.Group()
+    def load_image(name, colorkey=None):
+        fullname = os.path.join(name)
+        # если файл не существует, то выходим
+
+        image = pygame.image.load(fullname)
+        return image
+
+    sprite = pygame.sprite.Sprite()
+    sprite.image = load_image("image11.bmp")
+    sprite.rect = sprite.image.get_rect()
+    all_sprites.add(sprite)
+    for y in range(0, 14):
+        for x in range(0, 12):
+            x0 = x * w - (y % 2) * hw - 1
+            y0 = y * h - 1
+            pygame.draw.rect(screen, pygame.Color("white"), (x0, y0, w, h))
+            pygame.draw.rect(screen, pygame.Color("red"), (x0 + 1, y0 + 1, w - 2, h - 2))
+    pygame.display.flip()
+
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    pygame.quit()
